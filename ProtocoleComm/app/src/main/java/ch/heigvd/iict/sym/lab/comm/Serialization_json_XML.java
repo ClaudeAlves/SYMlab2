73e75182ed413513e58d1a3e58c519a223c0fba3
ClaudeAlves <claudeuw@hotmail.com>
2019-10-18 11:04:44 +0200

    activities creation

---
+++ ProtocoleComm/app/src/main/java/ch/heigvd/iict/sym/lab/comm/Serialization_json_XML.java
@@ -0,0 +1,14 @@
+package ch.heigvd.iict.sym.lab.comm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Serialization_json_XML extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_serialization_json__xml);
+    }
+}
