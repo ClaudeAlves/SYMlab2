e2b08acee00c656906cac96d9e185e9be279f683
Ichimizu <arzur.cateltorres@heig-vd.ch>
2019-11-01 10:28:01 +0100

    local

--- ProtocoleComm/app/src/main/java/ch/heigvd/iict/sym/lab/comm/Asynchronous.java
+++ ProtocoleComm/app/src/main/java/ch/heigvd/iict/sym/lab/comm/Asynchronous.java
@@ -2,13 +2,20 @@ package ch.heigvd.iict.sym.lab.comm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.app.Activity;
 import android.os.Bundle;
 
 public class Asynchronous extends AppCompatActivity {
+
+    final Activity asynch_activity = this;
+    final ActionBar actionBar = getActionBar();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asynchronous);
+        asynch_activity.setTitle("Asynchronous");
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
