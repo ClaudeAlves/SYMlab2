c9cce67f5524fc0b13401a2d94fc8ddd3257558c
Ichimizu <arzur.cateltorres@heig-vd.ch>
2019-11-08 14:19:46 +0100

    refactor async

--- ProtocoleComm/app/src/main/java/ch/heigvd/iict/sym/lab/comm/Serialization_json_XML.java
+++ ProtocoleComm/app/src/main/java/ch/heigvd/iict/sym/lab/comm/Serialization_json_XML.java
@@ -11,7 +11,84 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import static java.sql.DriverManager.println;
+public class Serialization_json_XML extends AppCompatActivity implements View.OnClickListener {
+
+    private User user;
+
+    private EditText editTextUsername, editTextPassword, editTextResponse;
+    private Button buttonLogin;
+
+    private final Gson gson = new GsonBuilder().create();
+
+    private final String SERVER_URL =  "http://sym.iict.ch/rest/json";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_serialization_json__xml);
+
+        //Binding the components with the view
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword=findViewById(R.id.editTextPassword);
+        editTextResponse=findViewById(R.id.editTextResponse);
+
+        buttonLogin=findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == buttonLogin){
+            user = new User(editTextUsername.getText().toString(), editTextPassword.getText().toString());
+            String serializedUser = gson.toJson(user);
+            sendRequest(serializedUser, SERVER_URL);
+        }
+    }
+
+    public boolean verifyServerResponse(String response){
+        User tmp = gson.fromJson(response, User.class);
+
+        if(tmp.equals(user)){
+            Toast.makeText(getApplicationContext(),"Serialization is finished: success",Toast.LENGTH_LONG).show();
+            return true;
+        }else{
+            Toast.makeText(getApplicationContext(),"Serialization is finished: failure",Toast.LENGTH_LONG).show();
+            return false;
+        }
+    }
+
+    public void sendRequest(String request, String url){
+        final AsynchRequest asynchHandler = new AsynchRequest();
+
+        asynchHandler.setCommunicationEventListener(response -> {
+            verifyServerResponse(response);
+            editTextResponse.setVisibility(View.VISIBLE);
+            editTextResponse.setText(response);
+            return true;
+        });
+
+        asynchHandler.execute(request, url, "text/plain");
+    }
+}
+
+
+
+/*
+
+package ch.heigvd.iict.sym.lab.comm;
+
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.Toast;
+
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import com.google.gson.Gson;
+        import com.google.gson.GsonBuilder;
+
+        import static java.sql.DriverManager.println;
 
 public class Serialization_json_XML extends AppCompatActivity implements View.OnClickListener {
 
@@ -72,3 +149,4 @@ public class Serialization_json_XML extends AppCompatActivity implements View.On
         asynchHandler.execute(request, url, "text/plain");
     }
 }
+*/
